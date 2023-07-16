b3b6d440b5c228b49f5d83719bd2e653c20002aa
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-16 16:02:34 +0300

    Замутил текст

--- lua/ju_pigeon_mail/derma/cl_ju_pigeon_mail_letter.lua
+++ lua/ju_pigeon_mail/derma/cl_ju_pigeon_mail_letter.lua
@@ -3,10 +3,40 @@ local letterMat = Material 'ju_pigeon_mail/letter.vmt'
 local letterMatSize = 2048
 
 local textColor = Color(56, 37, 5)
+local placeholderColor = Color(76, 62, 39, 130)
 local dColor = Color(255, 255, 255, 255)
+local cursorColor = Color(40, 40, 40)
+
+-- paddigs
+local p1 = 64
+
+DEFINE_BASECLASS 'DTextEntry'
+local PANEL = {}
+
+function PANEL:Init()
+
+    self:SetPaintBackground(false)
+    self:SetMultiline(true)
+    self:SetDrawLanguageID(false)
+    self:SetTabbingDisabled(false)
+
+    self:SetPlaceholderText 'Write, bro...'
+    self:SetPlaceholderColor(placeholderColor)
+
+    self:SetFont 'juPigeonMailLetterMainFont'
+    self:SetTextColor(textColor)
+
+end
+
+function PANEL:GetCursorColor()
+    return cursorColor
+end
+
+vgui.Register('juPigeonMailLetterTextEntry', PANEL, 'DTextEntry')
+
 
 DEFINE_BASECLASS 'EditablePanel'
-local PANEL = {}
+PANEL = {}
 
 function PANEL:Init()
     
@@ -16,6 +46,12 @@ function PANEL:Init()
     self:SetSize(w, h)
     self:Center()
 
+    self:MakePopup()
+
+    self.textPanel = vgui.Create('juPigeonMailLetterTextEntry', self)
+    self.textPanel:SetSize(w - p1 * 2, h - p1 * 2)
+    self.textPanel:Center()
+
 end
 
 function PANEL:Paint(w, h)
@@ -23,8 +59,6 @@ function PANEL:Paint(w, h)
     surface.SetMaterial(letterMat)
     surface.SetDrawColor(dColor)
     surface.DrawTexturedRect(0, 0, w, h)
-
-    draw.DrawText('Test', 'juPigeonMailLetterMainFont', 0, 0, textColor)
 
 end
 
