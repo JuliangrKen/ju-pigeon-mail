bc0509bf035ca1c6ed304157af23c5f19719fddd
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-16 14:07:07 +0300

    Добавил свепы с базой

---
+++ lua/weapons/ju_pigeon_mail_tame_base/shared.lua
@@ -0,0 +1,40 @@
+
+-- Q-menu
+SWEP.Category = ju.pigeon_mail.cfg.q_menu_category
+SWEP.Author = 'jujaken'
+SWEP.Spawnable = false
+
+-- Selector
+SWEP.Slot = 2
+SWEP.SlotPos = 0
+
+-- Hud
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = false
+
+
+function SWEP:Initialize()
+
+    
+
+end
+
+function SWEP:PrimaryAttack()
+    if !IsFirstTimePredicted() then return end
+
+    print 'PrimaryAttack'
+end
+
+function SWEP:SecondaryAttack()
+    if !IsFirstTimePredicted() then return end
+
+    print 'SecondaryAttack'
+end
+
+function SWEP:CanPrimaryAttack()
+    
+end
+
+function SWEP:CanSecondaryAttack()
+    
+end
---
+++ lua/weapons/ju_pigeon_mail_tame_pigeon/shared.lua
@@ -0,0 +1,8 @@
+
+SWEP.Base = 'ju_pigeon_mail_tame_base'
+
+-- Q-menu
+SWEP.PrintName = 'Лютый голубь'
+SWEP.Author = 'jujaken'
+SWEP.Category = ju.pigeon_mail.cfg.q_menu_category
+SWEP.Spawnable = true
